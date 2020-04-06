ebc08933f2d220cdeb7d2bf89f6c3a0ef500c06b
Zoe <Carpenter>
2020-04-06 14:56:54 -0400

    Initial Commit

---
+++ TrafficSegue2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegue2
+//
+//  Created by Soft Dev on 4/6/20.
+//  Copyright © 2020 Soft Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
